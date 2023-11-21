f710b9906fcd805dc12f8a8a937ee7616ccbc2a4
RklmpAndre <andre2016mendes@gmail.com>
2023-11-21 20:31:50 -0300

    q#	modified:   AppMercadoBD/nbproject/private/private.xml

--- AppMercadoBD/src/persist/HistoricoTO.java
+++ AppMercadoBD/src/persist/HistoricoTO.java
@@ -1,9 +1,22 @@
-package TO;
+package persist;
 
-/**
- *
- * @author 08050431
- */
+import entity.Produto;
+import java.util.List;
+import java.util.Objects;
+
 public class HistoricoTO {
     
-}
+    private List<Produto> produtos;
+    private int carrinhoId, produtoId, quantidade, itensCarrinhoId, formaPagamento, entrega;
+    private String dataEntrega, endereco, dataCompra;
+    private double valor;
+    
+
+    public HistoricoTO(String dataCompra, int entrega, int formaPagamento, double totalCompra, List<Produto> produtos, String endereco, String dataEntrega) {
+    }
+
+    @Override
+    public String toString() {
+        return "HistoricoTO{" + "cpf=" + getCpf() + ", dataColeta=" + getDataColeta() + ", altura=" + getAltura() + ", peso=" + getPeso() + ", medidaCintura=" + getMedidaCintura() + ", pressaoMinima=" + getPressaoMinima() + ", pressaoMaxima=" + getPressaoMaxima() + '}';
+    }
+    
